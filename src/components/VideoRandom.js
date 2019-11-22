44032c89061202d36199e6b66873b933e8e70df5
Cindie Jouvin <cindie.jouvin@gmail.com>
2019-11-22 10:13:52 +0100

    video

--- src/components/VideoRandom.js
+++ src/components/VideoRandom.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './VideoZombie.css'
+import './VideoGhost.css'
 
 function VideoRandom (){
     return(
     <div className="videoZombie">
        
-       <iframe width="952" height="535" src="https://www.youtube.com/embed/videoseries?list=PL60B29F446889D1BE&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       <iframe src="https://www.youtube.com/embed/videoseries?list=PL60B29F446889D1BE&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
     )
 }
